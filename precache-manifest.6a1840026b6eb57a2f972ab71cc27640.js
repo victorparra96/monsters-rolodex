841fe2471ca444af92c5f7cc459c435876a9863d
victorparra <victorhugo9620@hotmail.com>
2020-01-13 10:41:22 -0500

    Updates

---
+++ precache-manifest.6a1840026b6eb57a2f972ab71cc27640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9feed9acb38a517be8a223594f29a48e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ccc611528a88d16719b2",
+    "url": "/monsters-rolodex/static/css/main.90caf074.chunk.css"
+  },
+  {
+    "revision": "35e4a6c7caa4dac6614c",
+    "url": "/monsters-rolodex/static/js/2.4fe17a37.chunk.js"
+  },
+  {
+    "revision": "ccc611528a88d16719b2",
+    "url": "/monsters-rolodex/static/js/main.362a4b9c.chunk.js"
+  },
+  {
+    "revision": "98d8082a6d69a151432c",
+    "url": "/monsters-rolodex/static/js/runtime-main.217b21ad.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
